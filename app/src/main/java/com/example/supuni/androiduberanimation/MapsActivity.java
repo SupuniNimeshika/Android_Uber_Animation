3e6897992e000b5e255169e043fea11b3ce172ae
Your Name <supuni.nimeshika@gmail.com>
2018-03-09 21:14:40 +0530

    Implemented Code

--- app/src/main/java/com/example/supuni/androiduberanimation/MapsActivity.java
+++ app/src/main/java/com/example/supuni/androiduberanimation/MapsActivity.java
@@ -1,27 +1,77 @@
 package com.example.supuni.androiduberanimation;
 
+import android.os.Handler;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.example.supuni.androiduberanimation.Remote.IGoogleApi;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    SupportMapFragment mapFragment;
+    private List<LatLng> polylineList;
+    private Marker marker;
+    private float v;
+    private double lat,lng;
+    private Handler handler;
+    private LatLng startPosition,endPosition;
+    private int index,next;
+    private Button btnGo;
+    private EditText edtPlace;
+    private String destination;
+    private PolylineOptions polylineOptions,blackPolylineOption;
+    private Polyline blackPolyline,greyPolyline;
+    private LatLng myLocation;
+
+    IGoogleApi mService;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+        mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
+
+        polylineList =new ArrayList<>();
+        btnGo =(Button)findViewById(R.id.btnSearch);
+        edtPlace =(EditText)findViewById(R.id.edtPlace);
+        btnGo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                destination =edtPlace.getText().toString();
+                destination =destination.replace(" ","+");//replace space to + to make url
+                mapFragment.getMapAsync(MapsActivity.this);
+            }
+        });
+
+        mService =Common.getGoogleApi();
     }
 
 
@@ -29,9 +79,68 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
+        mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+        mMap.setTrafficEnabled(false);
+        mMap.setIndoorEnabled(false);
+        mMap.setBuildingsEnabled(false);
+        mMap.getUiSettings().setZoomControlsEnabled(true);
+
         // Add a marker in Sydney and move the camera
         LatLng sydney = new LatLng(16.0659970, 108.212552);
         mMap.addMarker(new MarkerOptions().position(sydney).title("My Location"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        mMap.moveCamera(CameraUpdateFactory.newCameraPosition(new CameraPosition.Builder()
+            .target(googleMap.getCameraPosition().target)
+            .zoom(17)
+            .bearing(30)
+            .tilt(45)
+            .build()));
+
+        String requestUrl =null;
+        try {
+
+            requestUrl ="https://maps.googleapis.com/maps/api/directions/json?"+
+                    "mode=driving&"+
+                    "transit_routing_preference=less_driving&"+
+                    "origin"+sydney.latitude+","+sydney.longitude+"&"+
+                    "destination="+destination+"&"+
+                    "key="+getResources().getString(R.string.google_directions_key);
+
+            Log.d("URL",requestUrl);//print url to review by chrome
+            mService.getDataFromGoogleApi(requestUrl)
+                    .enqueue(new Callback<String>() {
+                        @Override
+                        public void onResponse(Call<String> call, Response<String> response) {
+
+                            try {
+                                JSONObject jsonObject =new JSONObject(response.body().toString());
+                                JSONArray jsonArray =jsonObject.getJSONArray("routes");
+                                for(int i=0;i<jsonArray.length();i++)
+                                {
+                                    JSONObject route = jsonArray.getJSONObject(i);
+                                    JSONObject poly =route.getJSONObject("overview_polyline");
+                                    String polyline =poly.getString("points");
+                                    polylineList =decodePoly(polyline);
+                                }
+                            }catch (Exception e)
+                            {
+                                e.printStackTrace();
+                            }
+                        }
+
+                        @Override
+                        public void onFailure(Call<String> call, Throwable t) {
+                            Toast.makeText(MapsActivity.this,""+t.getMessage(),Toast.LENGTH_SHORT).show();
+                        }
+                    });
+
+        }catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    private List<LatLng> decodePoly(String polyline) {
+        //we can get this method on internet
     }
 }
